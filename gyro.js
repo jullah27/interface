c0e88db6194b94596f7ff8164df524824cd9781a
jullah27 <121064010+jullah27@users.noreply.github.com>
2022-12-21 11:14:05 +0100

    Update gyro.js

--- gyro.js
+++ gyro.js
@@ -1,5 +1,5 @@
 let sensorsWorking=false;
-//making sliders if no acc
+//making slider values
 let sliderY;
 let sliderX;
 //fake Acc value for testing, replace with real Acc
@@ -7,25 +7,25 @@ let fakeAccA = 270;
 let fakeAccB= 100;
 // var for accX & accY
 let accX;
-let accY;
+let accY = window.innerWidth/2;
 //var for current state of Acc
 let accAX;
 let accAY;
-fakeAccB = AccY;
+fakeAccB = accY;
 //calc and store half of Screen
 let mScreenW = window.innerWidth/2;
 let mScreenH = window.innerHeight/2;
 //define var for filling of circle
-let gyroFill = fakeAcc;
+let arcFill = 0.5;
 
 //make canvas, store var with init value
 function setup () {
     createCanvas (window.innerWidth, window.innerHeight);
-    sliderY = createSlider(0, 100, 50);
+    sliderY = createSlider(0, window.innerWidth, window.innerWidht/2);
     sliderY.position(0, window.innerHeight);
     sliderY.style('width', '80px');
     sliderX = createSlider(0, 100, 50);
-    slicerX.position(window.innerWidth/2,windwo.innerHeigth);
+    sliderX.position(window.innerWidth/2,window.innerHeigth);
     sliderX.style('width', '80px');
     //check if sensors are available 
     if(window.DeviceMotionEvent != undefined) {
@@ -42,14 +42,18 @@ function draw (){
     background(00,200,200);
     //make circle in the middle of screen
     fill(50,100,0);
-    circle (accB,window.innerHeight/2,100);
+    let fakAccY= sliderY.value();
+    circle (fakAccY,window.innerHeight/2,100);
+    print(sliderY.value());
+    print(sliderY.value());
     //def var which cut the first border
-    let rotCirc = 2;
+    let fakAccX=(sliderX.value()/50)*PI-1/2*PI;
+    let rotCirc = fakAccX;
     // def var which fill the circle in a equal way
-    let scndFill = rotCirc*1.4;
+    let scndFill = fakAccX*1.4;
     //make shape for the filled circle
     fill(255,0,0);
-    arc(accB,fakeAccB, 100, 100, rotCirc, scndFill);
+    arc(fakAccY,window.innerHeight/2, 100, 100, -1/2*PI, fakAccX);
     //
 
 
