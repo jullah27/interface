7cc50717dbdc969ea3767fd75ccb0918f02655b5
jullah27 <121064010+jullah27@users.noreply.github.com>
2023-01-11 13:23:08 +0100

    Create receiver.js

---
+++ receiver.js
@@ -0,0 +1,21 @@
+const socket = new WebSocket('wss://avior.uber.space/gyrointerface/receiver');
+//const socket = new WebSocket('ws://localhost:8000/receiver');
+
+const meanDot = document.querySelector('.dot.mean');
+
+// listen to connection open
+socket.addEventListener('open', (event) => {
+  setInterval(() => {
+    if (socket.readyState == socket.OPEN) {
+      socket.send('');
+    }
+  }, 30000);
+});
+
+// listen to message from server
+socket.addEventListener('message', (event) => {
+  const mean = JSON.parse(event.data);
+  meanDot.style.left = `${100 * mean[0]}%`;
+  meanDot.style.top = `${100 * mean[1]}%`;
+  window.max.outlet('mean', mean[0], mean[1]);
+});
